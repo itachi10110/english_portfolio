df9896fc6d2c2a077b6a855b51a6bb60748660ce
itachi10110 <130030034+itachi10110@users.noreply.github.com>
2024-01-20 20:52:13 +0100

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -19,16 +19,14 @@ const Layout = ({ children }) => {
     >
       {/* metadata */}
       <Head>
-        <title>Ethan Smith | Portfolio</title>
+        <title>добро пожаловать в мое портфолио</title>
         <meta
           name="description"
-          content="Ethan Smith is a Full-stack web developer with 10+ years of experience."
-        />
+          />
         <meta
           name="keywords"
           content="react, next, nextjs, html, css, javascript, js, modern-ui, modern-ux, portfolio, framer-motion, 3d-website, particle-effect"
         />
-        <meta name="author" content="Sanidhya Kumar Verma" />
         <meta name="theme-color" content="#f13024" />
       </Head>
 
