5bbcba7c371f6035d74453d0dbf2b539710d1c46
itachi10110 <130030034+itachi10110@users.noreply.github.com>
2024-01-20 18:38:29 +0100

    Update index.jsx

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -20,7 +20,7 @@ const Work = () => {
               exit="hidden"
               className="h2 xl:mt-12"
             >
-              My work <span className="text-accent">.</span>
+              My Certificates <span className="text-accent">.</span>
             </motion.h2>
             <motion.p
               variants={fadeIn("up", 0.4)}
@@ -29,8 +29,7 @@ const Work = () => {
               exit="hidden"
               className="mb-4 max-w-[400px] mx-auto lg:mx-0"
             >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              tempora eligendi, quisquam tempore sint reiciendis.
+              Those felt like a trophy when I got them , just like a proof , or even a sign that I can make it one day.
             </motion.p>
           </div>
 
