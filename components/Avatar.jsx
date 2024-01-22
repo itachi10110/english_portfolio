1a6ddb415a225e160f54155ed17a25acd158e6b3
itachi10110 <130030034+itachi10110@users.noreply.github.com>
2024-01-22 01:37:31 +0100

    Update Avatar.jsx

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -6,6 +6,8 @@ const Avatar = () => {
       <Image
         src="/avatar.png"
         alt="avatar"
+        height="350"
+        width="350"
         className="translate-z-0 w-full h-full"
       />
     </div>
