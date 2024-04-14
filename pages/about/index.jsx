24345b3fd2c9e1b5a9bd90565ca93e59e7b28392
itachi10110 <130030034+itachi10110@users.noreply.github.com>
2024-04-14 20:07:25 +0100

    Update index.jsx

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -141,7 +141,7 @@ const About = () => {
               {/* clients */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={10} duration={9} />
+                  <CountUp start={0} end={29} duration={10} />
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Certificates I Earned Through Courses.
